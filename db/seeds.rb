c87adb8d63626aecede92016b90557672b212d28
Joshua Davey <josh@joshuadavey.com>
2013-02-07 13:40:14 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+cat = User.find_or_create_by_name "Cat Stevens"
+felionel  = User.find_or_create_by_name "Felionel Richie"
+nelly  = User.find_or_create_by_name "Nelly Purrtado"
+
+Conversation.destroy_all
+
+Conversation.create! do |c|
+  c.from = felionel
+  c.to = cat
+  c.subject = "Hello. Is it me you're looking fur?"
+
+  c.messages.build do |m|
+    m.user = felionel
+    m.body = "I think I may be the cat you're looking for."
+  end
+
+  c.messages.build do |m|
+    m.user = cat
+    m.body = "Wow. I really don't know how to respond to that."
+  end
+
+  c.messages.build do |m|
+    m.user = felionel
+    m.body = "Say you. Say meow."
+  end
+end
+
+Conversation.create! do |c|
+  c.from = nelly
+  c.to = cat
+  c.subject = "I'm like a bird"
+
+  c.messages.build do |m|
+    m.user = nelly
+    m.body = "Except that I'm a cat"
+  end
+end
+
+Conversation.create! do |c|
+  c.from = cat
+  c.to = felionel
+  c.subject = "Another Caturday Night"
+
+  c.messages.build do |m|
+    m.user = cat
+    m.body = "And I ain't got nobody"
+  end
+
+  c.messages.build do |m|
+    m.user = felionel
+    m.body = "I'm sorry that you're feeling that way"
+  end
+end
